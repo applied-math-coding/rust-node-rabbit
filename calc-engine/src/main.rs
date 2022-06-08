cf319375306551548dd1f56f3cbd3e1793afd39a
applied.math.coding@gmail.com <applied.math.coding@gmail.com>
2022-06-08 12:08:33 +0200

    thread-pool in calc-engine

--- calc-engine/src/main.rs
+++ calc-engine/src/main.rs
@@ -3,13 +3,20 @@ use amiquip::{
     AmqpProperties, Connection, ConsumerMessage, ConsumerOptions, Exchange, Publish,
     QueueDeclareOptions, Result,
 };
+use num_cpus;
 use serde_json::json;
 
 fn main() -> Result<()> {
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(num_cpus::get())
+        .build()
+        .unwrap();
+
     //TODO take url from env
+    //TODO handle the error, wait 2 secs and then try to reconnect
+    //TODO when connection is closed, do break from loop below and re-run this method
     let mut connection = Connection::insecure_open("amqp://guest:guest@localhost:5672")?;
     let channel = connection.open_channel(None)?;
-    let exchange = Exchange::direct(&channel);
     let queue = channel.queue_declare("hanoi", QueueDeclareOptions::default())?;
     let consumer = queue.consume(ConsumerOptions::default())?;
     for message in consumer.receiver().iter() {
@@ -22,16 +29,23 @@ fn main() -> Result<()> {
                 ) {
                     (Some(r), Some(c)) => (r.clone(), c.clone()),
                     _ => {
-                        panic!("received delivery without reply_to or correlation_id");
+                        println!("received delivery without reply_to or correlation_id");
+                        continue;
                     }
                 };
-                exchange.publish(Publish::with_properties(
-                    json!(hanoi::hanoi(body.parse().unwrap(), 0, 2))
-                        .to_string()
-                        .as_bytes(),
-                    reply_to,
-                    AmqpProperties::default().with_correlation_id(correlation_id),
-                ))?;
+                let channel_for_msg = connection.open_channel(None)?;
+                pool.spawn(move || {
+                    let exchange = Exchange::direct(&channel_for_msg);
+                    exchange
+                        .publish(Publish::with_properties(
+                            json!(hanoi::hanoi(body.parse().unwrap(), 0, 2))
+                                .to_string()
+                                .as_bytes(),
+                            reply_to,
+                            AmqpProperties::default().with_correlation_id(correlation_id),
+                        ))
+                        .unwrap();
+                });
                 consumer.ack(delivery)?;
             }
             other => {
