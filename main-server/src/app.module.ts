1401cab1059924a3c0ec8bef73455e285ffb5160
applied.math.coding@gmail.com <applied.math.coding@gmail.com>
2022-06-13 14:28:36 +0200

    docker

--- main-server/src/app.module.ts
+++ main-server/src/app.module.ts
@@ -3,12 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessageService } from './message.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '../.env',
-    })
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client/dist'),
+    }),
   ],
   controllers: [AppController],
   providers: [
@@ -16,4 +21,4 @@ import { MessageService } from './message.service';
     MessageService
   ],
 })
-export class AppModule {}
+export class AppModule { }
